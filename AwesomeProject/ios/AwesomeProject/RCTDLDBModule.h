61666d74d25e7f6d2bc523297ed026231ede8e25
Christophe <christophe@dldb.io>
2022-01-25 13:00:40 +0100

    direct integration of DLDB SDK 0.9.6 into react-native app

---
+++ AwesomeProject/ios/AwesomeProject/RCTDLDBModule.h
@@ -0,0 +1,10 @@
+//created by christophe@dld.io
+
+#ifndef RCTDLDBModule_h
+#define RCTDLDBModule_h
+
+#import <React/RCTBridgeModule.h>
+@interface RCTDLDBModule : NSObject <RCTBridgeModule>
+@end
+
+#endif /* RCTDLDBModule_h */
---
+++ AwesomeProject/screens/TabOneScreen.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+import React from 'react';
+import { NativeModules, Button } from 'react-native';
+const { DLDBModule } = NativeModules;
+
+const onPress = () => {
+  DLDBModule.addEventsWithoutLocation('{"button" : "pressed"}');
+};
+
+export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab One</Text>
+      <Button
+            onPress={onPress}
+            title="Press Me"
+            color="#1e90ff"
+          />
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
